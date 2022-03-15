3e0943b14d2c2aa4660966240377d626ecce8759
Keith <keithvip@gmail.com>
2022-03-15 14:34:59 -0500

    beinghelped

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,12 @@
 var generateBtn = document.querySelector("#generate");
 
 var possibleCharacters = {
-  promptLength: "8,10",
-  promptNumerical: "1,2,3,4,5,6,7,8,9,10",
-  prompText: "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p",
-  promptCharacters: "!,@,#,$,%,^,&,*"
-}
+   numeric: "12345678910",
+   lowercase: "abcdefghijklmnop",
+   uppercase:  "ABCDEFGHIJKLMNOP",
+   special: "!@#$%^&*"
+ }
+
 
 
 var randomNumber = function (min, max) {
@@ -20,91 +21,39 @@ var randomNumber = function (min, max) {
 
 // Write password to the #password input
 function writePassword() {
-  window.alert("Generate Password?");
-  var possibleChars = [];
+  var possibleChars;
 
   var promptLength = window.prompt("Please enter 8 or 10 for password length");
   if (promptLength === "" || promptLength === null) {
     window.alert("You need to provide a valid answer! Please try again.");
     // use return to call it again and stop the rest of this function from running
-    return writePassword();
+    promptLength = window.prompt("Please enter 8 or 10 for password length");
   }
-  promptLength = promptLength.toLowerCase();
-  
-  if (promptLength === "no") {
-    window.alert("Text Values Omitted");
-     //no need to subtract from the array, since it started off emptyu.
+  var promptNumerical = window.confirm("Do you want numerical characters?");
+  var promptLower = window.confirm("Do you want lowercase characters?");
+  var promptUpper = window.confirm("Do you want uppercase characters?");
+  var promptSpecial = window.confirm("Do you want special characters?");
+
+
+  if (promptNumerical) {
+    possibleChars.concat(possibleCharacters.numeric);
+    console.log(possibleChars);
   }
-  if (promptLength === "yes") {
-    window.alert("Text Values Added");
-    possibleChars.push(possibleCharacters.promptLength);
-    for(var i =0; i = promptLength.length; i++) {
-      console.log(promptLength[i])
-    }
-   //here we add something into the array.
-  };
-
-  var promptNumerical = window.prompt("Do you want numerical characters? Please enter Yes or No.");
-
-  if (possibleChars === "" || possibleChars === null) {
-    window.alert("You need to provide a valid answer! Please try again.");
-    
-    return writePassword();
+  if (promptLower) {
+   possibleChars.concat(possibleCharacters.lowercase) 
+   console.loc(possibleChars);
   }
-  promptNumerical = promptNumerical.toLowerCase();
-
-  if (promptNumerical === "no") {
-    window.alert("Numeric Values Omitted");
-    
-
+  if (promptUpper) {
+    possibleChars.concat(possibleCharacters.uppercase) 
+    console.loc(possibleChars);
   }
-
-  if (promptNumerical === "yes") {
-    window.alert("Numeric Values Added");
-    possibleChars.push(possibleCharacters.promptNumerical)
-  };
-
-  var promptText = window.prompt("Do you want text characters? Please enter Yes or No.");
-
-  if (promptText === "" || promptText === null) {
-    window.alert("You need to provide a valid answer! Please try again.");
-   
-    return writePassword();
+  if (promptSpecial) {
+    possibleChars.concat(possibleCharacters.special) 
+    console.loc(possibleChars);
   }
-  promptText = promptText.toLowerCase();
-
-  if (promptText === "no") {
-    window.alert("Text Values Omitted"); 
-  }
-  if (promptText === "yes") {
-    window.alert("Text Values Added");
-    possibleChars.push(possibleCharacters.prompText)
-  };
-
-  var promptCharacters = window.prompt("Do you want specials characters? Please enter Yes or No.");
-
-  if (promptCharacters === "" || promptCharacters === null) {
-    window.alert("You need to provide a valid answer! Please try again.");
-    
-    return writePassword();
-  }
-
-  promptCharacters = promptCharacters.toLowerCase();
-
-  if (promptCharacters === "no") {
-    window.alert("Character Values Omitted");
-   
-
-  }
-
-  if (promptCharacters === "yes") {
-    window.alert("Character Values Added");
-    possibleChars.push(possibleCharacters.promptCharacters)
-
-    return possibleCharacters();
+    return;
   
   }
-};
 
 
 //I fixed this up for you, and put this separate displayPassword function definition here.
